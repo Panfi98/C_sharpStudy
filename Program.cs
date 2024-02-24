b68907133bc9e3cfc6806c5034cba8563b797d5c
Daniil Panfilov <danielpanf@gmail.com>
2024-02-24 23:08:03 +0100

    Added som initial code, implemented lists.

--- Program.cs
+++ Program.cs
@@ -4,3 +4,8 @@ and presents it nicelly in a table. The end table presents the exam scores and g
 
 Feature1> The app should include extra credits to calculate final gpa.
 */
+
+int currentAssignments = 5;
+
+int[] sophiaScore = new int[] { 90, 86, 87, 98, 100};
+int[] andrewScore = new int[] { 92, 89, 81, 96, 90};
