3282a7005a92910b5aa502ba3948b9ca8f55384d
Daniil Panfilov <danielpanf@gmail.com>
2024-02-24 18:39:05 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿/* 
+This application calculates students gpa from their exam results
+and presents it nicelly in a table. The end table presents the exam scores and gpa. 
+
+Feature1> The app should include extra credits to calculate final gpa.
+*/
