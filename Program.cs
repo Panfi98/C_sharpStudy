724d90f1443e09027193cd186bb0f77e0711deff
Panfi98 <49882563+Panfi98@users.noreply.github.com>
2024-02-25 18:08:08 +0100

    updated discription of the code

--- Program.cs
+++ Program.cs
@@ -1,22 +1,15 @@
 ﻿/* 
-This C# console application is designed to:
-- Use arrays to store student names and assignment scores.
-- Use a `foreach` statement to iterate through the student names as an outer program loop.
-- Use an `if` statement within the outer loop to identify the current student name and access that student's assignment scores.
-- Use a `foreach` statement within the outer loop to iterate though the assignment scores array and sum the values.
-- Use an algorithm within the outer loop to calculate the average exam score for each student.
-- Use an `if-elseif-else` construct within the outer loop to evaluate the average exam score and assign a letter grade automatically.
-- Integrate extra credit scores when calculating the student's final score and letter grade as follows:
-    - detects extra credit assignments based on the number of elements in the student's scores array.
-    - divides the values of extra credit assignments by 10 before adding extra credit scores to the sum of exam scores.
-- use the following report format to report student grades: 
+This application calculates students gpa from their exam results
+and presents it nicelly in a table. The end table presents the exam scores and gpa. 
 
     Student         Grade
 
-    Sophia:         92.2    A-
-    Andrew:         89.6    B+
-    Emma:           85.6    B
-    Logan:          91.2    A-
+Student         Exam score      Overall Grade   Extra Credit
+
+Sophia          92,2            95,88   A       92 (3,68 pts)
+Andrew          89,6            91,38   A-      89 (1,78 pts)
+Emma            85,6            90,94   A-      85 (5,34 pts)
+Logan           91,2            93,12   A       91 (1,92 pts)
 */
 int examAssignments = 5;
 
